3837449a9355038c19828bda69a05fda0e4463ad
TROGDOR\dough <doughtnerd@gmail.com>
2017-07-19 21:13:42 -0600

    Game works

---
+++ Assets/Scripts/CollectableItems/Promotion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Grind
+{
+    public class Promotion : CollectableItem
+    {
+        [SerializeField]
+        private float multiplier = 2f;
+
+        public static event Action<float> PromotionCollected;
+
+        protected override void OnCollect()
+        {
+            if (PromotionCollected != null)
+            {
+                PromotionCollected(multiplier);
+            }
+        }
+    }
+}
